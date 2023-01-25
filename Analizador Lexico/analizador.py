32cc17f715da8a23559a2601c4409cf4589397d9
franco-e-s-c <113216065+franco-e-s-c@users.noreply.github.com>
2023-01-25 00:04:21 -0600

    Update analizador.py

--- Analizador Lexico/analizador.py
+++ Analizador Lexico/analizador.py
@@ -2,6 +2,9 @@ class miniLexico:
     def __init__(self, cadena):
         self.cadena = cadena + '$'
 
+    def comprobar(self):
+        pass
+
     def analizar(self):
         identificador = ""
         numero = ""
@@ -12,6 +15,12 @@ class miniLexico:
         n=0
         skip = False
         tipo = ["int", "void", "float"]
+        simbolos = [";", ",", "(", ")", "{", "}", "$"]
+        tipoA = ["12", "13", "14", "15", "16", "17", "23"]
+        relac = ""
+        orV = ""
+        reservadas = ["while", "return", "else"]
+        tipoR = ["20", "21", "22"]
         for caracter in self.cadena:
             asCarac = ord(caracter)
             if (caracter == '$'):
@@ -24,19 +33,19 @@ class miniLexico:
                     skip=False
             elif (asCarac > 64 and asCarac < 91) or (asCarac>96 and asCarac<123): #Comprobamos si el caracter es una letra
                 if(len(real)!=0):
-                    print("Real: ", real)
+                    print("Real Tipo 2: ", real)
                     real=""
                     identificador=""
                     numero=""
                     identificador += caracter
                 elif(len(numero)!=0):
-                    print("Entero: ", numero)
+                    print("Entero Tipo 1: ", numero)
                     real=""
                     identificador=""
                     numero=""
                     identificador += caracter
                 elif(len(cadenaS)!=0):
-                    print("Cadena: ", cadenaS)
+                    print("Cadena Tipo 3: ", cadenaS)
                     real=""
                     identificador=""
                     numero=""
@@ -49,18 +58,18 @@ class miniLexico:
                 if(len(identificador)!=0):
                     identificador += caracter
                     if(identificador in tipo):
-                        print("Tipo: ", identificador)
+                        print("TipoDato Tipo 4: ", identificador)
                         identificador=""
-                    #print("identificador", identificador)
-                    #real=""
-                    #identificador=""
-                    #numero=""
-                #if(len(real)!=0):
-                    #real += caracter
-                #elif(len(identificador)!=0):
-                    #identificador += caracter
+
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
                 elif(len(cadenaS)!=0):
-                    print("Cadena: ", cadenaS)
+                    print("Cadena Tipo 3: ", cadenaS)
                     real=""
                     identificador=""
                     numero=""
@@ -81,25 +90,33 @@ class miniLexico:
             elif (caracter == " "):
                 if(len(identificador)!=0):
                     if(identificador in tipo):
-                        print("Tipo: ", identificador)
+                        print("TipoDato Tipo 4: ", identificador)
                         identificador=""
+
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
                     else:
-                        print("Identificador: ", identificador)
+                        print("Identificador Tipo 0: ", identificador)
                         real=""
                         identificador=""
                         numero=""
                 elif(len(real)!=0):
-                    print("Real: ", real)
+                    print("Real Tipo 2: ", real)
                     real=""
                     identificador=""
                     numero=""
                 elif(len(numero)!=0):
-                    print("Entero: ", numero)
+                    print("Entero Tipo 1: ", numero)
                     real=""
                     identificador=""
                     numero=""
                 elif(len(cadenaS)!=0):
-                    print("Cadena: ", cadenaS)
+                    print("Cadena Tipo 3: ", cadenaS)
                     real=""
                     identificador=""
                     numero=""
@@ -111,20 +128,28 @@ class miniLexico:
                 cont = 0
                 if(len(identificador)!=0):
                     if(identificador in tipo):
-                        print("Tipo: ", identificador)
+                        print("TipoDato Tipo 4: ", identificador)
                         identificador=""
+
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
                     else:
-                        print("Identificador: ", identificador)
+                        print("Identificador Tipo 0: ", identificador)
                         real=""
                         identificador=""
                         numero=""
                 elif(len(real)!=0):
-                    print("Real: ", real)
+                    print("Real Tipo 2: ", real)
                     real=""
                     identificador=""
                     numero=""
                 elif(len(numero)!=0):
-                    print("Entero: ", numero)
+                    print("Entero Tipo 1: ", numero)
                     real=""
                     identificador=""
                     numero=""
@@ -137,7 +162,7 @@ class miniLexico:
                         break
                     elif (self.cadena[cadenaCar]=="'") or (self.cadena[cadenaCar]=='"'):
                         skip = True
-                        print("Cadena: ", cadenaS)
+                        print("Cadena Tipo 3: ", cadenaS)
                         real=""
                         identificador=""
                         numero=""
@@ -145,34 +170,286 @@ class miniLexico:
                         break
                     cadenaS=cadenaS + self.cadena[cadenaCar]
 
-
-
-
             elif (caracter == '+') or (caracter == '-'):
                 if(len(numero)!=0):
-                    print("Entero: ", numero)
+                    print("Entero Tipo 1: ", numero)
                     real=""
                     identificador=""
                     numero=""
-                    print("opSuma: ", caracter)
+                    print("opSuma Tipo 5: ", caracter)
                 elif(len(real)!=0):
-                    print("Real: ", real)
+                    print("Real Tipo 2: ", real)
                     real=""
                     identificador=""
                     numero=""
-                    print("opSuma: ", caracter)
+                    print("opSuma Tipo 5: ", caracter)
                 elif(len(identificador)!=0):
                     if(identificador in tipo):
-                        print("Tipo: ", identificador)
+                        print("TipoDato Tipo 4: ", identificador)
+                        print("opSuma Tipo 5: ", caracter)
                         identificador=""
+
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
                     else:
-                        print("Identificador: ", identificador)
+                        print("Identificador Tipo 0: ", identificador)
                         real=""
                         identificador=""
                         numero=""
-                        print("opSuma: ", caracter)
+                        print("opSuma Tipo 5: ", caracter)
                 else:
-                    print("opSuma: ", caracter)
+                    print("opSuma Tipo 5: ", caracter)
+            
+            elif (caracter == '*') or (caracter == '/'):
+                if(len(numero)!=0):
+                    print("Entero Tipo 1: ", numero)
+                    real=""
+                    identificador=""
+                    numero=""
+                    print("opMul Tipo 6: ", caracter)
+                elif(len(real)!=0):
+                    print("Real Tipo 2: ", real)
+                    real=""
+                    identificador=""
+                    numero=""
+                    print("opMul Tipo 6: ", caracter)
+                elif(len(identificador)!=0):
+                    if(identificador in tipo):
+                        print("TipoDato Tipo 4: ", identificador)
+                        print("opMul Tipo 6: ", caracter)
+                        identificador=""
+
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
+                    else:
+                        print("Identificador Tipo 0: ", identificador)
+                        real=""
+                        identificador=""
+                        numero=""
+                        print("opMul Tipo 6: ", caracter)
+                else:
+                    print("opMul Tipo 6: ", caracter)
+
+            elif (caracter == '<') or (caracter == '>') or (caracter == '!'):
+                if(len(numero)!=0):
+                    print("Entero Tipo 1: ", numero)
+                    real=""
+                    identificador=""
+                    numero=""
+                elif(len(real)!=0):
+                    print("Real Tipo 2: ", real)
+                    real=""
+                    identificador=""
+                    numero=""
+                elif(len(identificador)!=0):
+                    if(identificador in tipo):
+                        print("TipoDato Tipo 4: ", identificador)
+                        identificador=""
+
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
+                    else:
+                        print("Identificador Tipo 0: ", identificador)
+                        real=""
+                        identificador=""
+                        numero=""
+                if (self.cadena[i+1]=="="):
+                    cont= 1
+                    skip = True
+                    relac = caracter + self.cadena[i+1]
+                    print("opRelac Tipo 7: ", relac)
+                else:
+                    print("opRelac Tipo 7: ", caracter)
+
+            elif (caracter == '='):
+                if(len(numero)!=0):
+                    print("Entero Tipo 1: ", numero)
+                    real=""
+                    identificador=""
+                    numero=""
+                elif(len(real)!=0):
+                    print("Real Tipo 2: ", real)
+                    real=""
+                    identificador=""
+                    numero=""
+                elif(len(identificador)!=0):
+                    if(identificador in tipo):
+                        print("TipoDato Tipo 4: ", identificador)
+                        identificador=""
+
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
+                    else:
+                        print("Identificador Tipo 0: ", identificador)
+                        real=""
+                        identificador=""
+                        numero=""
+                if (self.cadena[i+1]=="="):
+                    relac = caracter + self.cadena[i+1]
+                    print("opRelac Tipo 7: ", relac)
+                else:
+                    print("opAsignacion Tipo 18: ", caracter)
+                
+            elif (caracter == '|'):
+                if(len(numero)!=0):
+                    print("Entero Tipo 1: ", numero)
+                    real=""
+                    identificador=""
+                    numero=""
+                elif(len(real)!=0):
+                    print("Real Tipo 2: ", real)
+                    real=""
+                    identificador=""
+                    numero=""
+                elif(len(identificador)!=0):
+                    if(identificador in tipo):
+                        print("TipoDato Tipo 4: ", identificador)
+                        identificador=""
+
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
+                    else:
+                        print("Identificador Tipo 0: ", identificador)
+                        real=""
+                        identificador=""
+                        numero=""
+                if (self.cadena[i+1]=="|"):
+                    orV = caracter + self.cadena[i+1]
+                    print("opOr Tipo 8: ", orV)
+                else:
+                    print("OpOr incompleto ")
+
+            elif (caracter == '&'):
+                if(len(numero)!=0):
+                    print("Entero Tipo 1: ", numero)
+                    real=""
+                    identificador=""
+                    numero=""
+                elif(len(real)!=0):
+                    print("Real Tipo 2: ", real)
+                    real=""
+                    identificador=""
+                    numero=""
+                elif(len(identificador)!=0):
+                    if(identificador in tipo):
+                        print("TipoDato Tipo 4: ", identificador)
+                        identificador=""
+
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
+                    else:
+                        print("Identificador Tipo 0: ", identificador)
+                        real=""
+                        identificador=""
+                        numero=""
+                if (self.cadena[i+1]=="&"):
+                    andV = caracter + self.cadena[i+1]
+                    print("opAnd Tipo 9: ", andV)
+                else:
+                    print("OpAnd incompleto ")
+
+            elif (caracter == '!'):
+                if(len(numero)!=0):
+                    print("Entero Tipo 1: ", numero)
+                    real=""
+                    identificador=""
+                    numero=""
+                    print("opNot Tipo 10: ", caracter)
+                elif(len(real)!=0):
+                    print("Real Tipo 2: ", real)
+                    real=""
+                    identificador=""
+                    numero=""
+                    print("opNot Tipo 10: ", caracter)
+                elif(len(identificador)!=0):
+                    if(identificador in tipo):
+                        print("TipoDato Tipo 4: ", identificador)
+                        print("opNot Tipo 10: ", caracter)
+                        identificador=""
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
+                    else:
+                        print("Identificador Tipo 0: ", identificador)
+                        real=""
+                        identificador=""
+                        numero=""
+                        print("opNot Tipo 10: ", caracter)
+                else:
+                    print("opNot Tipo 10: ", caracter)
+
+            elif (caracter in simbolos):
+                it = 0
+                for it in range(0, len(simbolos)):
+                    if caracter == simbolos[it]:
+                        tipoS = tipoA[it]
+                    it= it+1
+
+                if(len(numero)!=0):
+                    print("Entero Tipo 1: ", numero)
+                    real=""
+                    identificador=""
+                    numero=""
+                elif(len(real)!=0):
+                    print("Real Tipo 2: ", real)
+                    real=""
+                    identificador=""
+                    numero=""
+                elif(len(identificador)!=0):
+                    if(identificador in tipo):
+                        print("TipoDato Tipo 4: ", identificador)
+                        identificador=""
+
+                    elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
+                        
+                    else:
+                        print("Identificador Tipo 0: ", identificador)
+                        real=""
+                        identificador=""
+                        numero=""
+                print("Simbolo Tipo "+ tipoS + ":", caracter)
+             
+            
             i=i+1
 
                     
@@ -191,6 +468,14 @@ class miniLexico:
             if(identificador in tipo):
                 print("Tipo: ", identificador)
                 identificador=""
+
+            elif (identificador in reservadas):
+                        it = 0
+                        for it in range(0, len(reservadas)):
+                            if identificador == reservadas[it]:
+                                tipoS = tipoR[it]
+                            it= it+1 
+                        print("Reservada Tipo "+ tipoS + ":", identificador)
             else:
                 print("Identificador", identificador)
                 real=""
@@ -201,3 +486,4 @@ class miniLexico:
 print("Ingresa una cadena")
 cadena = input()
 miniLexico(cadena).analizar()
+
